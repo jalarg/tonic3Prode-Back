7b047150b5027928c2329a24b44b024980234799
RosannaContasti <rosicontasti@gmail.com>
2023-03-24 12:22:16 -0300

    cambios

--- controllers/tournaments.js
+++ controllers/tournaments.js
@@ -1,37 +1,87 @@
 const { Tournaments } = require("../db_models");
-const tournamentCopaArgentina = require("../seed/tournamentCA");
 
 module.exports = {
-  getAll: async (req, res) => {
-    const tournament = await Tournaments.find().populate("teams");
-    res.send(tournament);
+  getAll: async (req, res, next) => {
+    try {
+      const tournaments = await Tournaments.find().populate("teams");
+      res.send(tournaments);
+    } catch (err) {
+      next(err);
+    }
   },
-  createTournament: async (req, res) => {
-    const { active, beggining, ending, title, details, type } = req.body;
-    const newTournament = new Tournaments({
-      active: tournamentCopaArgentina.active,
-      beggining: tournamentCopaArgentina.beggining,
-      ending: tournamentCopaArgentina.ending,
-      title: tournamentCopaArgentina.title,
-      details: tournamentCopaArgentina.details,
-      type: tournamentCopaArgentina.type,
-    });
-    await newTournament.save();
-    res.send("Creado");
-  },
-  assingTeams: async (req, res) => {
+  getOne: async (req, res, next) => {
     const { _id } = req.params;
-    const { team } = req.body;
+    try {
+      const tournament = await Tournaments.findById(_id);
+      res.send(tournament);
+    } catch (err) {
+      next(err);
+    }
+  },
+  createTournament: async (req, res, next) => {
+    try {
+      const { active, beggining, ending, title, details, type } = req.body;
+      const newTournament = new Tournaments({
+        active,
+        beggining,
+        ending,
+        title,
+        details,
+        type,
+      });
+      await newTournament.save();
+      res.send("Creado");
+    } catch (err) {
+      next(err);
+    }
+  },
+  assingTeams: async (req, res, next) => {
+    try {
+      const { _id } = req.params;
+      const { team } = req.body;
 
-    const tournamentsUpdate = await Tournaments.findByIdAndUpdate(_id, {
-      $push: { teams: team },
-    });
-    res.send(tournamentsUpdate);
+      const tournamentsUpdate = await Tournaments.findByIdAndUpdate(_id, {
+        $push: { teams: team },
+      });
+      res.send(tournamentsUpdate);
+    } catch (err) {
+      next(err);
+    }
   },
-  deleteOne: async (req, res) => {
+  updateOne: async (req, res, next) => {
     const { _id } = req.params;
-    console.log(_id)
-    const removed = await Tournaments.findByIdAndDelete(_id);
-    res.send(`Deleted from database`);
+    const { active, beggining, ending, title, details, teams, type } = req.body;
+    try {
+      const updateTournament = await Tournaments.findOneAndUpdate(_id, {
+        active,
+        beggining,
+        ending,
+        title,
+        details,
+        teams,
+        type,
+      });
+      res.send("Tournament was updated");
+    } catch (err) {
+      next(err);
+    }
+  },
+  deleteOne: async (req, res, next) => {
+    try {
+      const { _id } = req.params;
+      console.log(_id);
+      const removed = await Tournaments.findByIdAndDelete(_id);
+      res.send(`Deleted from database`);
+    } catch (err) {
+      next(err);
+    }
+  },
+  deleteAll: async (req, res, next) => {
+    try {
+      const removeAll = await Tournaments.deleteMany();
+      res.send(`Deleted from database`);
+    } catch (err) {
+      next(err);
+    }
   },
 };
--- seed/tournamentCA.js
+++ seed/tournamentCA.js
@@ -1,8 +1,8 @@
 const tournamentCopaArgentina = 
   {
     active: true,
-    beggining: "24/01/2023",
-    ending: "31/12/2023",
+    beggining: "2023/01/24",
+    ending: "2023/12/31",
     title: "Copa Argentina",
     details:
       "La Copa Argentina es un torneo de fútbol que se disputa anualmente en Argentina. El objetivo del torneo es determinar al campeón del fútbol argentino de la temporada, otorgándole al ganador un cupo en la Copa Libertadores de América, el torneo más prestigioso de clubes en Sudamérica.",
