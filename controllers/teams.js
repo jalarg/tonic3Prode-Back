75ebdb06df39ae2b68141553c82c735a016147e6
jalarg <jlema1990@gmail.com>
2023-03-27 00:23:37 -0300

    hotfix

--- controllers/teams.js
+++ controllers/teams.js
@@ -12,6 +12,16 @@ module.exports = {
     }
   },
 
+  searchTeam: async (req, res, next) => {
+    try {
+      const { name } = req.params;
+      const team = await Teams.findOne({ name: { $eq: name } }).exec();
+      res.send(team);
+    } catch (err) {
+      next(err);
+    }
+  },
+
   // RUTAS PARA UPDATE RAPIDO CON SEED ARMADO
 
   updateTeams: async (req, res, next) => {
@@ -22,7 +32,6 @@ module.exports = {
       next(err);
     }
   },
-
 
   // RUTAS CON PERMISOS DE ADMINISTRADOR
 
@@ -35,18 +44,9 @@ module.exports = {
     if (user.rol !== "superAdmin" && user.rol !== "admin") {
       return res.status(403).send("You are not allowed to do this action");
     }
-
-  searchTeam: async (req, res, next) => {
-    try {
-      const { name } = req.params;
-      const team = await Teams.findOne({ name: { $eq: name } }).exec();
-      res.send(team);
-    } catch (err) {
-      next(err);
-    }
   },
-  updateOneTeam: async (req, res, next) => {
 
+  updateOneTeam: async (req, res, next) => {
     try {
       const newTeam = new Teams(team);
       const savedTeam = await newTeam.save();
@@ -55,6 +55,7 @@ module.exports = {
       next(err);
     }
   },
+
   updateOneTeam: async (req, res, next) => {
     const teamId = req.params.id;
     const { updates, uid } = req.body;
@@ -85,6 +86,7 @@ module.exports = {
       next(err);
     }
   },
+
   deleteTeams: async (req, res, next) => {
     const { uid } = req.body;
     const user = await Users.findOne({ uid });
@@ -101,6 +103,7 @@ module.exports = {
       next(err);
     }
   },
+
   deleteOneTeam: async (req, res, next) => {
     const { uid } = req.body;
     const user = await Users.findOne({ uid });
